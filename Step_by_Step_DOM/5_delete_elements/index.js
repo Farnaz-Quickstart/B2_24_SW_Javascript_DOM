0ab0e0697b967b53598f553f5106e28cf36e0821
farnaz <farnaz.towhidi@gmail.com>
2024-10-30 10:32:27 -0400

    event handlling

--- Step_by_Step_DOM/5_delete_elements/index.js
+++ Step_by_Step_DOM/5_delete_elements/index.js
@@ -9,14 +9,14 @@ function addElements() {
 }
 
 function deleteElements () {
-  let image_elements = document.getElementsByTagName ("img")
-  for (let i = image_elements.length ; i > 0 ;i--) {
-    image_elements[0].remove();
-  }
-
-  // for (let i = 0 ; i <  image_elements.length ;i++) {
-  //   image_elements[i].remove();
+   let image_elements = document.getElementsByTagName ("img")
+  // for (let i = image_elements.length ; i > 0 ;i--) {
+  //   image_elements[0].remove();
   // }
+
+  for (let i = 0 ; i <  image_elements.length ;i++) {
+    image_elements[i].remove();
+  }
 }
 
 
--- Step_by_Step_DOM/6_event_handling/script.js
+++ Step_by_Step_DOM/6_event_handling/script.js
@@ -1,15 +1,39 @@
 // addEventListener: 
 // Reference: https://www.w3schools.com/jsref/dom_obj_event.asp
 
-function handleClick_submit () {
-  console.log("Hello, World!");
+document.getElementById("btn_submit2").addEventListener ("click", doSomething())
+
+
+function handleClick_submit1 () {
+  console.log ("Hello 1")
 }
 
+function handleClick_submit2 () {
+  console.log ("Hello 2")
+}
 
-let mybutton = document.getElementById('btn_submit2');
-mybutton.style.backgroundColor = "red"
-mybutton.addEventListener ("click", handleClick_submit)
-mybutton.addEventListener ("click", ()=>{console.log ("arrow function")})
+function handleClick_mouseOver() {
+  // alert ("mouseOver")
+}
+
+function doSomething() {
+  console.log ("mytisha")
+  alert ("hello")
+  document.getElementById("btn_submit1").style.backgroundColor = "blue"
+  document.getElementById("btn_submit3").remove()
+  let headerTemp = document.createElement ("h2")
+  headerTemp.innerText = "haha"
+  // document.getElementById ("testDiv").appendChild (headerTemp)
+}
+ 
+let mitisha = document.getElementById("btn_submit2")
+console.log (mitisha)
+mitisha.style.backgroundColor= "red"
+// mitisha.addEventListener("click", handleClick_submit);
+
+
+let btn_submit3 = document.getElementById("btn_submit3")
+btn_submit3.addEventListener("mouseover", handleClick_mouseOver);
 
 
 
